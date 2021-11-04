6c8692a1e5853bb2b41be16ca277ade27e049d65
ComplexOri <jeremoi.jansson@gmail.com>
2021-11-04 15:15:02 +0200

    inheritance
    -inheritance
    -little of abstraction

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+public void ChangeTextRed()
+    {
+        Debug.Log("Working here Red!");
+    }
+
+    public void ChangeTextGreen()
+    {
+        Debug.Log("Working here Green!");
+    }
+
+    public void ChangeTextBlue()
+    {
+        Debug.Log("Working here Blue!");
+    }
+}
